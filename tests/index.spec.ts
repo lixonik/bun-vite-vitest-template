73e2a32cb2d04fe59a15f049cab8370d6e878a4f
Boris Prokhorov <deimos.75@mail.ru>
2024-05-16 19:28:28 +0300

    test case naming fix

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,7 +2,7 @@ import { greet } from '../src'
 import { expect, suite, test } from 'vitest'
 
 suite('greet function', () => {
-	test('greet function', () => {
+	test('should greet the world', () => {
 		expect(greet('World')).toEqual('Hello, World!')
 	})
 })
